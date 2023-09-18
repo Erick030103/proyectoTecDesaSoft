fc3884a77988463eac8af9cdf813195ab3bb4a43
Erick Acuña <erick.acunat@gmail.com>
2023-09-18 17:53:52 -0600

    Se conecta a render

--- src/db.js
+++ src/db.js
@@ -2,12 +2,9 @@ import cliente from 'pg'
 
 const { Pool } = cliente;
 
-const pool = new Pool ({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'claseTecFront',
-  password: 'Eridan03',
-  port: 5432
+const pool = new Pool ({ 
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 
 export{
